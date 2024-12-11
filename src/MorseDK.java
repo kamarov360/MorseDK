14b57c2e9dc11dc4ce5df87a8289b613f37117cc
kamarov360 <kamarov360@gmail.com>
2024-12-11 19:45:43 +0100

    Dodanie instrukcji switch do menu wyboru

--- src/MorseDK.java
+++ src/MorseDK.java
@@ -9,27 +9,45 @@ public class MorseDK
 
     public static void main(String[] args)
     {
+        Scanner wejscie = new Scanner(System.in);
         System.out.println(autor);
         System.out.println("1. Przerób tekst na kod Morse'a.");
         System.out.println("2. Przerób kod Morse'a na tekst.");
         System.out.println("3. Pomoc.");
         System.out.println("4. Wyjście z programu.");
-        Scanner wejscie = new Scanner(System.in);
         try
         {
             int wybor = wejscie.nextInt();
+            switch (wybor)
+            {
+                case 1:
+                    System.out.println("opcja 1");
+                    break;
+                case 2:
+                    System.out.println("opcja 2");
+                    break;
+                case 3:
+                    System.out.println("opcja 3");
+                    break;
+                case 4:
+                    System.out.println("opcja 4");
+                    break;
+                    default:
+                        System.out.println("Nieprawidłowe dane switch.");
+                        break;
+            }
         }
         catch (InputMismatchException e)
         {
             System.out.println("Podałeś nieprawidłową opcję.");
         }
     }
-    public static void tekstNaMorse(String text)
-    {
+    //public static void tekstNaMorse(String text)
+    //{
 
-    }
-    public static void morseNaTekst(String text)
-    {
+    //}
+    //public static void morseNaTekst(String text)
+    //{
 
-    }
+    //}
 }
