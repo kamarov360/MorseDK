60439f8ebd6cd6e0ede1f6959520ead3af8ef342
kamarov360 <kamarov360@gmail.com>
2024-12-11 17:26:08 +0100

    Stworzenie klasy main, stworzenie zmiennej stałej autor, wywołanie w klasie main zmiennej autor

---
+++ src/MorseDK.java
@@ -0,0 +1,9 @@
+public class MorseDK
+{
+    public static final String autor = "Program stworzony przez Kubę Witkowskiego. Społeczna Akademia Nauk w Łodzi, kierunek - Informatyka.\nNumer albumu - 106263, grupa - IV, semestr - I.";
+
+    public static void main(String[] args)
+    {
+        System.out.println(autor);
+    }
+}
