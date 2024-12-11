c4f2e7a1bfb675165a3d1ac25f8126e6633869bc
kamarov360 <kamarov360@gmail.com>
2024-12-11 17:37:04 +0100

    Stworzenie opcji wyboru w postaci string, oczekiwanie na dane wejściowe od użytkownika dot. menu

--- src/MorseDK.java
+++ src/MorseDK.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MorseDK
 {
     public static final String autor = "Program stworzony przez Kubę Witkowskiego. Społeczna Akademia Nauk w Łodzi, kierunek - Informatyka.\nNumer albumu - 106263, grupa - IV, semestr - I.";
@@ -5,5 +7,11 @@ public class MorseDK
     public static void main(String[] args)
     {
         System.out.println(autor);
+        System.out.println("1. Przerób tekst na kod Morse'a.");
+        System.out.println("2. Przerób kod Morse'a na tekst.");
+        System.out.println("3. Pomoc.");
+        System.out.println("4. Wyjście z programu.");
+        Scanner wejscie = new Scanner(System.in);
+        int wybor = wejscie.nextInt();
     }
 }
