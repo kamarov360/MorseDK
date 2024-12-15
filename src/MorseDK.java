6b637cf9e5dfd545c22a7db38560d17dbba71e02
kamarov360 <kamarov360@gmail.com>
2024-12-15 19:12:11 +0100

    Dodanie komentarzy do wszystkich linii w klasie MorseDK

--- src/MorseDK.java
+++ src/MorseDK.java
@@ -4,18 +4,18 @@ import java.util.InputMismatchException;//import klasy InputMismatchException do
 import java.util.Scanner;//import klasy Scanner, potrzebnej do wykorzystania w celu odczytania danych z konsoli od użytkownika.
 public class MorseDK //deklaracja publicznej klasy MorseDK.
 {
-    private final String autor = "Program stworzony przez Kubę Witkowskiego. Społeczna Akademia Nauk w Łodzi, kierunek - Informatyka.\nNumer albumu - 106263, grupa - IV, semestr - I.";
+    private final String autor = "Program stworzony przez Kubę Witkowskiego. Społeczna Akademia Nauk w Łodzi, kierunek - Informatyka.\nNumer albumu - 106263, grupa - IV, semestr - I.";//Deklaracja zmiennej autor, jest dostępna tylko wewnątrz klasy. Ma wartość stałą, jej typ danych to String, czyli łańcuch tekstu.
 
-    public static void main(String[] args)
+    public static void main(String[] args)//Metoda main jest punktem wejścia do programu, który jest wywoływany przez JVM.
     {
-        MorseDK obj = new MorseDK();
+        MorseDK obj = new MorseDK();//Ta linia tworzy nowy obiekt klasy MorseDK i przypisuje jego referencję do zmiennej obj. Obiekt ten będzie używany do interakcji z metodami i właściwościami klasy MorseDK
         System.out.println(obj.autor);//Wyświetla tekst w konsoli na podstawie zmiennej prywatnej autor. Inicjalizacja możliwa dzięki powołaniu się na obiekt.
 
         Scanner scanner = new Scanner(System.in);//Scanner to klasa wbudowana w bibliotekę Java. "scanner" to nazwa zmiennej obiektowej, używam tej zmiennej do wywoływania metod klasy Scanner. "new" Tworzy nowy obiekt klasy Scanner. Scanner jest skonstruowany z System.in, będzie odczytywał dane wpisane przez użytkownika z klawiatury.
 
-        while (true)
+        while (true)//Nieskończona pętla while, pętla posiada warunek true. Dzięki niej program działa cały czas, chyba że użytkownik zakończy działanie, wybierając opcję 4 z menu.
         {
-            try
+            try//Blok try jest używany do "opakowania" kodu, który może wygenerować wyjątek (błąd) w trakcie wykonania.
             {
                 System.out.println("\nMenu:");//Wyświetla użytkownikowi napis "Menu:" i przechodzi do nowej linii. Znak \n pomaga w stworzeniu pustej linii po wyświetleniu zawartości zmiennej autor.
                 System.out.println("1. Pomoc");//Wyświetla użytkownikowi napis "1. Pomoc" i przechodzi do nowej linii.
@@ -38,12 +38,12 @@ public class MorseDK //deklaracja publicznej klasy MorseDK.
                     case 2: //wybór 2
                         System.out.print("Podaj kod Morse'a (znaki rozdzielone spacją): ");//Wyświetla napis w konsoli.
                         String kodMorse = scanner.nextLine();//Program przypisuje całą wczytaną linię (jako ciąg znaków String) do zmiennej "kodMorse".
-                        System.out.println("\nTekst na podstawie kodu: " + KonwerterMorse.przerobNaMorse(kodMorse));
+                        System.out.println("\nTekst na podstawie kodu: " + KonwerterMorse.przerobNaMorse(kodMorse));//wywołanie metody przerobNaMorse() z klasy KonwerterMorse, przekazywany argument to kodMorse czyli tekst w kodzie Morse wprowadzony przez użytkownika.
                         break; //program wychodzi tylko z switch i wraca na początek pętli while (ponieważ jej warunek true nadal jest spełniony).
                     case 3: //wybór 3
                         System.out.print("Podaj tekst do konwersji na kod Morse'a: ");//Wyświetla napis w konsoli.
                         String tekst = scanner.nextLine();//Program przypisuje całą wczytaną linię (jako ciąg znaków String) do zmiennej "tekst".
-                        System.out.println("\nKod Morse'a na podstawie tekstu: " + KonwerterMorse.przerobNaTekst(tekst));
+                        System.out.println("\nKod Morse'a na podstawie tekstu: " + KonwerterMorse.przerobNaTekst(tekst));//wywołanie metody przerobNaTekst() z klasy KonwerterMorse, przekazywany argument to tekst po przerobieniu z kodu Morse który wprowadził wcześniej użytkownik.
                         break; //program wychodzi tylko z switch i wraca na początek pętli while (ponieważ jej warunek true nadal jest spełniony).
                     case 4: //wybór 4
                         System.out.print("Dziękuję za skorzystanie z programu. Do widzenia.");//Wyświetla napis w konsoli.
